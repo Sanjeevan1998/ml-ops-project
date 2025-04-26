c97e80d96790bccf689d3f05c5e21e533a906290
Vishwas Karale <vishwaskarale83@gmail.com>
2025-04-26 18:36:14 -0400

    Github Actions variable issue fix

--- variables.tf
+++ variables.tf
@@ -7,15 +7,3 @@ variable "key_pair" {}
 variable "network_name" {}
 variable "image_name" {}
 variable "flavor_name" {}
-
-# terraform.tfvars.example
-# Copy this to terraform.tfvars and fill your secrets
-auth_url = "https://chi.uc.chameleoncloud.org:5000/v3"
-region_name = "CHI@UC"
-domain_name = "Default"
-application_credential_id = "your-app-cred-id"
-application_credential_secret = "your-app-cred-secret"
-key_pair = "your-keypair"
-network_name = "sharednet1"
-image_name = "CC-Ubuntu20.04"
-flavor_name = "m1.medium"
