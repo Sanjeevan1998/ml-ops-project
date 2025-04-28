6bfd27d8b6b488ecb75e6eb054750693d08bb1b9
Vishwas Karale <vishwaskarale83@gmail.com>
2025-04-28 11:55:27 -0400

    Terraform backend initialization

---
+++ terraform/backend/provider.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    openstack = {
+      source  = "terraform-provider-openstack/openstack"
+      version = "~> 1.49.0"
+    }
+  }
+}
+
+provider "openstack" {
+  auth_url                     = var.auth_url
+  region_name                  = var.region_name
+  
+  application_credential_id     = var.APPLICATION_CREDENTIAL_ID
+  application_credential_secret = var.APPLICATION_CREDENTIAL_SECRET
+}
