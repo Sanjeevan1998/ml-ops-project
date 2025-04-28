af43f50faa11429359a0c7516c3c7ce188856ee3
Vishwas Karale <vishwaskarale83@gmail.com>
2025-04-28 13:20:39 -0400

    Remvoing duplicate backends

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,17 +1,5 @@
 terraform {
   backend "swift" {
-    auth_url    = var.auth_url
-    user_name   = var.APPLICATION_CREDENTIAL_ID
-    password    = vvar.APPLICATION_CREDENTIAL_SECRET
-    tenant_name = var.tenant_name
-    region_name = var.region_name
-    container   = "terraform-tfstate"
-    object_name = "terraform.tfstate"
-  }
-}
-
-terraform {
-  backend "swift" {
     auth_url    = var.backend_auth_url          
     user_name   = var.CHITACC_APPLICATION_CREDENTIAL_ID     
     password    = var.CHITACC_APPLICATION_CREDENTIAL_SECRET 
