87a427cc35aeab6fb322ee9cfbe932e3fbda969a
Vishwas Karale <vishwaskarale83@gmail.com>
2025-04-30 23:52:00 -0400

    Making terraform stateless

--- terraform/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name   = "primary-resource"
-    storage_account_name  = "tfbackendmlopsgroup36"
-    container_name        = "terraform-state"
-    key                   = "terraform.tfstate" 
-  }
-}
