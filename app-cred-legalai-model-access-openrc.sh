5b2c424a3cf8d99a3d126f781575b8938e4097b7
Arnab Bhowal <arnabb1998@mac.mynetworksettings.com>
2025-05-10 12:59:00 -0400

    Added app-cred

---
+++ app-cred-legalai-model-access-openrc.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+export OS_AUTH_TYPE=v3applicationcredential
+export OS_AUTH_URL=https://chi.uc.chameleoncloud.org:5000/v3
+export OS_IDENTITY_API_VERSION=3
+export OS_REGION_NAME="CHI@UC"
+export OS_INTERFACE=public
+export OS_APPLICATION_CREDENTIAL_ID=3227d2cb68d54757a4daaa97eaf0c915
+export OS_APPLICATION_CREDENTIAL_SECRET=829LWgtXA7JoMPv-h-U8b-Bhl_jRrrMfeD7gIXlhQyX-gga1-vFLRFp2XXB4Kk2QX1qkSbAuHeOj-8Kn-fLJag
